a616096c2d6d8d467253b605b6d6f76786401851
umeryudattyuno <s1f102104045@iniad.org>
2024-04-19 09:28:13 +0900

    first completion

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.scss";
+import { API_KEY } from "./request";
+import YouTube from "react-youtube";
+import movieTrailer from 'movie-trailer';
 
 
 const base_url = "https://image.tmdb.org/t/p/original";
-
 
 type Props = {
   title: string;
@@ -21,34 +23,74 @@ type Movie = {
   backdrop_path: string;
 };
 
+type Options = {
+  height: string;
+  width: string;
+  playerVars: {
+    autoplay: 0 | 1 | undefined;
+  };
+};
 
 export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [trailerUrl, setTrailerUrl] = useState<string | null>(null);
 
   useEffect(() => {
     async function fetchData() {
-        const request = await axios.get(fetchUrl);
-        setMovies(request.data.results);
-        return request;
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
     }
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
+  const opts: Options = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = async (movie: Movie) => {
+    if (trailerUrl) {
+      setTrailerUrl(null);
+    } else {
+      try {
+        const trailerResponse = await axios.get(
+          `/movie/${movie.id}/videos?api_key=${API_KEY}`
+        );
+        const trailerId = trailerResponse.data.results[0]?.key;
+        if (trailerId) {
+          setTrailerUrl(trailerId);
+        } else {
+          movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+            .then((url: string) => {
+              const urlParams = new URLSearchParams(new URL(url).search);
+              setTrailerUrl(urlParams.get("v"));
+            })
+            .catch((error: any) => console.error('Error loading trailer:', error.message));
+        }
+      } catch (error) {
+        console.error('Error fetching trailer:', error);
+      }
+    }
+  };
 
   return (
     <div className="Row">
       <h2>{title}</h2>
       <div className="Row-posters">
-        {movies && movies.map((movie) => (  // データ存在のチェック
+        {movies.map((movie) => (
           <img
             key={movie.id}
             className={`Row-poster ${isLargeRow ? "Row-poster-large" : ""}`}
             src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
-    </div> 
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
   );
 };
